e871ee67a409ef6615f3e0f8cba7311fbb721895
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-09-28 17:30:22 +0530

    project hi-terrace

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Residencies from './components/Residencies/Residencies'
+import Value from './components/Value/Value'
+import Contact from './components/Contact/Contact'
+
+function App() {
+  return (
+    <>
+     <div className='App'>
+     <div>
+       <div className='white-gradiant'>
+         <Header/>
+         <Hero/>
+       </div>
+        <Residencies/>
+        <Value/>
+        <Contact />
+     </div>
+     </div>
+    </>
+  )
+}
+
+export default App
